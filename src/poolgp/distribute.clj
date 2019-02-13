3408ada91ae67ef2ca6fe566bbeaa66170febdf2
jackHay22 <jjphay@gmail.com>
2019-02-13 10:19:22 -0500

    clojush/poolgp integration testing updates

--- src/poolgp/distribute.clj
+++ src/poolgp/distribute.clj
@@ -13,6 +13,8 @@
 (def COMPLETED-CHAN (async/chan 1000))
 (def PENDING-DIST-CHAN (async/chan 1000))
 
+(def OPPONENT-POOL (atom nil))
+
 (defn- log
   [msg]
   (println "poolgp.distribute =>" msg))
@@ -55,24 +57,23 @@
   "take a socket and an individual and send"
   [host port]
   (reset! DIST-SERVER-STARTED? true)
+  (log "Starting distribution worker...")
   (async/go-loop []
-    (let [indiv (async/<! PENDING-DIST-CHAN)
+    (let [indiv (async/<!! PENDING-DIST-CHAN)
           client-socket (Socket. host port)]
       (with-open [writer (io/writer client-socket)]
         (.write writer (str (pr-str (make-indiv-packet indiv)) "\n"))
-        (.flush writer)
-        (.close client-socket)
-        ))))
+        (.flush writer))
+        (recur))))
 
 (defn- opp-pool-worker
   "start a thread that servers opponent pool requests"
-  [port opponents]
+  [port]
   ;TODO purge after cycle
   (reset! OPP-POOL-WORKER-STARTED? true)
-  (println "Setting opp pool of size: " (count opponents))
+  (log "Starting opponent pool worker...")
   (future
-    (let [socket (ServerSocket. port)
-          outgoing-ops (map make-opp-packet opponents (range))]
+    (let [socket (ServerSocket. port)]
       (.setSoTimeout socket 0)
       (loop []
         (let [client-socket (.accept socket)
@@ -80,24 +81,18 @@
             (try
               (doall (map
                 #(.write writer (str (pr-str %) "\n"))
-                          outgoing-ops))
+                      (map make-opp-packet @OPPONENT-POOL (range))))
               (catch Exception e
                 (.printStackTrace e)))
             (.flush writer)
             (.close client-socket))
       (recur)))))
 
-(defn- unpack-indiv
-  "take individuals returned from poolgp workers and strips off extra
-  information (returns clojush.individual)"
-  [poolgp-indiv]
-  ;TODO: aggregate fitness, etc...
-    (:indiv poolgp-indiv))
-
 (defn- incoming-socket-worker
   "start a listener for completed individuals"
   [port]
   (reset! INCOMING-WORKER-STARTED? true)
+  (log "Starting incoming socket worker...")
   (let [socket (ServerSocket. port)]
     (.setSoTimeout socket 0)
     (async/go-loop []
@@ -107,16 +102,12 @@
                 (catch SocketException e
                   nil))]
           (if (not (= ind nil))
-            (async/>! COMPLETED-CHAN (unpack-indiv ind)))
+            (async/>! COMPLETED-CHAN (read-string ind)))
           (recur)))))
 
 (defn- verify-indiv
   [indiv]
   )
-
-(defn- get-return-indiv
-  []
-  (async/go (async/<!! COMPLETED-CHAN)))
 
 (defn eval-indiv
   "take individual, opponent list,
@@ -125,26 +116,39 @@
   IN: (list clojush.indvidual)
   OUT: (list clojush.individual)
   "
-  [indiv opponents config]
+  [indiv]
     ;wait for connectivity
     ; (check-worker-status (:host config) (:outgoing-port config))
     ; (log "Connected to eval worker")
-    (async/go (async/>! PENDING-DIST-CHAN indiv))
-    ;set infinite timeout
-
-
-    ;worker that responds to opp pool requests
-    (if (not @OPP-POOL-WORKER-STARTED?)
-      (opp-pool-worker (:opp-pool-req-p config) opponents))
-
-    ;server worker that sends individuals out that arrive on outgoing channel
-    (if (not @DIST-SERVER-STARTED?)
-      (distribution-worker (:host config) (:outgoing-port config)))
-
-    ;task that listens for incoming individuals
-    (if (not @INCOMING-WORKER-STARTED?)
-      (incoming-socket-worker (:incoming-port config)))
+    (if (and @OPP-POOL-WORKER-STARTED?
+                  @DIST-SERVER-STARTED?
+                  @INCOMING-WORKER-STARTED?)
+        (async/go (async/>! PENDING-DIST-CHAN indiv))
+        (log "Error: one or more services not started before trying to distribute individuals! (use `(start-dist-services config)`)"))
 
     ;take an individual from the completed channel
-    (get-return-indiv)
-    (println "HERE"))
+    ;(blocking wait)
+    (async/<!! COMPLETED-CHAN))
+
+(defn register-opponents
+  "register opponents for a cycle"
+  [opponents]
+  (log "Registering opponent pool")
+  (reset! OPPONENT-POOL opponents)
+  (log "Updating cycle")
+  (swap! CURRENT-CYCLE inc))
+
+(defn start-dist-services
+  "start core services (once)"
+  [config]
+  ;worker that responds to opp pool requests
+  (if (not @OPP-POOL-WORKER-STARTED?)
+    (opp-pool-worker (:opp-pool-req-p config)))
+
+  ;server worker that sends individuals out that arrive on outgoing channel
+  (if (not @DIST-SERVER-STARTED?)
+    (distribution-worker (:host config) (:outgoing-port config)))
+
+  ;task that listens for incoming individuals
+  (if (not @INCOMING-WORKER-STARTED?)
+    (incoming-socket-worker (:incoming-port config))))
